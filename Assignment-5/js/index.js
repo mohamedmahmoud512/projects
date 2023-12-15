1d64bcde2baba309bac7c9bbca7e06f67ef132b9
mohamedmahmoud512 <117411857+mohamedmahmoud512@users.noreply.github.com>
2023-12-15 15:40:18 +0200

    JS Assignment -2

--- Assignment-5/js/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("reloadLink").addEventListener("click", function (reloud) {
-    reloud.preventDefault(); // Prevent the default link behavior
-    location.reload(); // Reload the page
-});
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
